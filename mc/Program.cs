4c8cd7b521fd8a25e06d1f0ee37eb36bfb86ec93
vince <vincepr@gmx.de>
2023-10-25 23:38:36 +0200

    hooked up binder to frontend

--- mc/Program.cs
+++ mc/Program.cs
@@ -1,11 +1,12 @@
 ﻿// https://www.youtube.com/watch?v=3XM9vUGduhk&list=PLRAdsfhKI4OWNOSfS7EUu5GRAVmze1t2y&index=2
 // 13:50
 using Minsk.CodeAnalysis;
+using Minsk.CodeAnalysis.Binding;
 using Minsk.CodeAnalysis.Syntax;
 
 namespace Minsk
 {
-    
+
     // 1 + 2 * 3
     // gets parsed into a treel like:
     //
@@ -19,8 +20,8 @@ namespace Minsk
     {
         private static void Main()
         {
-            Console.WriteLine("#showTree : to enable parse-tree information");
-            Console.WriteLine("#cls : clear the Terminal");
+            Console.WriteLine("type: '#showTree' \t: enable parse-tree information");
+            Console.WriteLine("type: '#cls' \t: clear the Terminal");
             bool showTree = false;
             while (true)
             {
@@ -34,38 +35,45 @@ namespace Minsk
                 if (line == "#showTree")
                 {
                     showTree = !showTree;
-                    Console.WriteLine(showTree ? "ENABLED showing parse-tree." : "disabled showing parse-tree");
+                    Console.WriteLine(showTree ? "ENABLED showing parse-tree." : "NOT showing parse-tree");
                     continue;
-                } else if (line == "#cls")
+                }
+                else if (line == "#cls")
                 {
                     Console.Clear();
                     continue;
                 }
 
-                // create tree
+                // create the syntax tree - 
                 var syntaxTree = SyntaxTree.Parse(line);
+                // next parse the syntax tree again and add some additional info like Types to it -> bound-tree
+                var binder = new Binder();
+                var boundExpression = binder.BindExpression(syntaxTree.Root);
+                var diagnostics = syntaxTree.Diagnostics.Concat(binder.Diagnostics).ToArray();
+
 
                 // optional showing of parse-tree
                 if (showTree)
                 {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                PrettyPrint(syntaxTree.Root);
-                Console.ResetColor();
-                }
-
-                // print diagnostics OR result
-                if (syntaxTree.Diagnostics.Any())
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    foreach (var d in syntaxTree.Diagnostics)
-                        Console.WriteLine(d);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    PrettyPrint(syntaxTree.Root);
                     Console.ResetColor();
                 }
-                else
+
+                // print the result of the evaluation
+                if (!diagnostics.Any())
                 {
-                    var eval = new Evaluator(syntaxTree.Root);
+                    var eval = new Evaluator(boundExpression);
                     var result = eval.Evaluate();
                     Console.WriteLine(result.ToString());
+                }
+                // print diagnostics instead if we encountered an ERROR   
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    foreach (var d in diagnostics)
+                        Console.WriteLine(d);
+                    Console.ResetColor();
                 }
             }
         }
@@ -74,7 +82,7 @@ namespace Minsk
         //   ├─PlusToken
         //   └─NumberExpression
         //     └─NumberToken 2
-        static void PrettyPrint( SyntaxNode node, string indent="", bool isLast=true)
+        static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = true)
         {
             Console.Write(indent);
             Console.Write(isLast ? "└─" : "├─");
@@ -84,11 +92,11 @@ namespace Minsk
                 Console.Write(" ");
                 Console.Write(t.Value);
             }
-            Console.WriteLine() ;
+            Console.WriteLine();
             indent += isLast ? "  " : "│ ";
-            var lastChild = node.GetChildren().LastOrDefault(); 
+            var lastChild = node.GetChildren().LastOrDefault();
             foreach (var child in node.GetChildren())
-                PrettyPrint(child, indent, child==lastChild);
+                PrettyPrint(child, indent, child == lastChild);
         }
     }
 }
